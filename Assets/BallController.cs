7e945eec0c55653489b082e93125b96be32e638a
zzmjeremy <zhuominzhucontact@gmail.com>
2025-01-25 11:13:01 -0800

    Final Output

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -18,22 +18,26 @@ public class BallController : MonoBehaviour
   if (Input.GetKey(KeyCode.W))
   {
   inputVector+=Vector2.up; //"a +=b"<=> "a= a+b"
-  }
+            Debug.Log("User's Input: up");
+        }
 
   if (Input.GetKey(KeyCode.S))
      {
      inputVector += Vector2.down;
-     }
+            Debug.Log("User's Input: down");
+        }
 
   if (Input.GetKey(KeyCode.D))
      {
      inputVector += Vector2.right;
-     }
+            Debug.Log("User's Input: right");
+        }
 
   if (Input.GetKey(KeyCode.A))
      {
      inputVector += Vector2.left;
-     }
+            Debug.Log("User's Input: left");
+        }
 
         Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
